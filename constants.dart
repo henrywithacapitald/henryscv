cf17a9931c35e33c573221c531a5e203120a3e8d
Henry <95125800+henrywithacapitald@users.noreply.github.com>
2021-12-20 01:36:56 +1300

    Add files via upload

---
+++ constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const TextStyle kRegularTextStyle =
+    TextStyle(fontFamily: "Bebas", fontSize: 25);
+
+const TextStyle kTitleTextStyle =
+    TextStyle(fontFamily: "Bebas", fontSize: 40, fontWeight: FontWeight.bold);
+
+const TextStyle kCardElementTextStyle =
+    TextStyle(fontSize: 20, fontFamily: "Bebas", color: Colors.blueGrey);
+
+const TextStyle kRegularSmallTextStyle = TextStyle(fontFamily: "Bebas", fontSize: 18.5);
+
+const TextStyle kBoldTextStyle =
+    TextStyle(fontFamily: "Bebas", fontWeight: FontWeight.bold, fontSize: 18.5);
